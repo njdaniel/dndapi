1d6d5f0db4d86cce66a9d3fbd4351c543c4cc1d7
njdaniel <nicholasjdaniel@gmail.com>
2021-11-05 09:53:06 -0400

    create character with new fields

--- models/character.go
+++ models/character.go
@@ -6,10 +6,13 @@ import (
 	"github.com/gobuffalo/validate/v3"
 	"github.com/gofrs/uuid"
 	"time"
+	"github.com/gobuffalo/validate/v3/validators"
 )
 // Character is used by pop to map your characters database table to your go code.
 type Character struct {
     ID uuid.UUID `json:"id" db:"id"`
+    Name string `json:"name" db:"name"`
+    Body string `json:"body" db:"body"`
     CreatedAt time.Time `json:"created_at" db:"created_at"`
     UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
@@ -32,7 +35,10 @@ func (c Characters) String() string {
 // Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
 // This method is not required and may be deleted.
 func (c *Character) Validate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
+	return validate.Validate(
+		&validators.StringIsPresent{Field: c.Name, Name: "Name"},
+		&validators.StringIsPresent{Field: c.Body, Name: "Body"},
+	), nil
 }
 
 // ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
